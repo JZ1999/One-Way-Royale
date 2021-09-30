e110f0c35643641f6b8eb2998acced32a15d826c
JZ1999 <joseph.zamora64@yahoo.com>
2021-09-29 22:15:08 -0600

    fix: Leaderboard fix

---
+++ Assets/Scripts/EditorUtilities.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class EditorUtilities : MonoBehaviour
+{
+	/// Add a context menu named "Do Something" in the inspector
+	/// of the attached script.
+	[ContextMenu("Erase PlayerPrefs")]
+	public void DoSomething()
+	{
+		Debug.Log("PlayerPrefs eliminated");
+		PlayerPrefs.DeleteAll();
+	}
+}
---
+++ Assets/Scripts/Models/LeaderboardEntriesModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class LeaderboardEntriesModel
+{
+	public int count;
+	public string next;
+	public string previous;
+	public LeaderboardEntryModel[] results;
+}
